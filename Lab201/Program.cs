6e0365bed068c3e1a4d28412ce3807540fa51e7c
CupcakeMonster <flukersy@gmail.com>
2015-08-28 23:59:19 +0700

    Week2 Lab2-1 Student.cs

--- Lab201/Program.cs
+++ Lab201/Program.cs
@@ -1,6 +1,8 @@
+﻿//Natthapong Burakitphachai 570611011 SEC003
+//ขอโทษนะครับ แต่ commit ไฟล์ student.cs ไม่ได้เพราะอาจารย์ลบไปแล้ว เลยขอรวมโคดไว้ในนี้แทน
 using System;
 
-namespace Lab2
+namespace Lab201
 {
 	class MainClass
 	{
@@ -21,12 +23,111 @@ namespace Lab2
 				Console.WriteLine (s);
 
 			Console.WriteLine ("\n--- Playing with s4, s5, s6 objects ---");
-			stds [4].Name = "Mario";
+			stds [4].Name = "Mario";        
 			stds [5].YearOfBirth = 1900;
 			stds [6].isActive = false;
 			for (int i = 4; i < stds.Length; i++)
 				Console.WriteLine (stds [i]);
 
 		}
-	}
+
+        class Student
+        {
+            private string Sname;
+            private string Sid;
+            private int Sage;
+            private int Sbirth;
+            private bool Sact;
+            private int CurYear = DateTime.Now.Year;
+
+
+            public Student()
+            {
+                this.Sname = "John Doe";
+                this.Sid = "Unknow";
+                this.Sact = true;
+                this.Sage = CurYear - 1995;
+            }
+
+            public Student(string Name, string StudentID)
+            {
+                this.Sname = Name;
+                this.Sid = StudentID;
+                this.Sact = true;
+                this.Sage = CurYear - 1995;
+            }
+
+            public Student(string Name, string StudentID, int YearOfBirth)
+            {
+                this.Sname = Name;
+                this.Sid = StudentID;
+                this.Sbirth = YearOfBirth;
+                this.Sact = true;
+                this.Sage = CurYear - YearOfBirth;
+            }
+
+            public Student(string Name, string StudentID, int YearOfBirth, bool isActive)
+            {
+                this.Sname = Name;
+                this.Sid = StudentID;
+                this.Sbirth = YearOfBirth;
+                this.Sact = isActive;
+                this.Sage = CurYear - YearOfBirth;
+            }
+
+            public string Name
+            {
+                get { return Sname; }
+                set
+                {
+                    Sname = value;
+                }
+            }
+
+            public int YearOfBirth
+            {
+                get { return Sbirth; }
+                set
+                {
+                    int testAge = Sage;
+                    this.Sage = CurYear - value;
+                    if ((Sage >= 50) || (Sage <= 50))
+                    {
+                        Console.WriteLine("{0}: error try setting invalid year-of-birth value!", this.Sid);
+                        Sage = testAge;
+                    }
+
+
+                }
+            }
+
+            public bool isActive
+            {
+                get { return Sact; }
+                set
+                {
+                    this.Sact = value;
+                }
+            }
+
+            public int getAge(int Iage)     //sry not implement but usable...
+            {
+
+                int age = CurYear - Iage;
+                return age;
+            }
+
+            public override string ToString()
+            {
+                if (Sact == true)
+                {
+                    return "[Student: " + Sname + " (" + Sid + "), age=" + Sage + ", is active student]";
+                }
+                else
+                {
+                    return "[Student: " + Sname + " (" + Sid + "), age=" + Sage + ", is NOT active student]";
+                }
+            }
+        }
+    }
 }
